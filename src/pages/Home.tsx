e33e067d6f327d2cdaa1aaf3bf85539f676f67ab
PAtreju <pq5978@gmail.com>
2024-08-07 17:58:09 +0200

    current day func

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,13 @@ import { FaCalendarAlt } from "react-icons/fa";
 import { BsFillHeartPulseFill } from "react-icons/bs";
 import { GiJugglingSeal } from "react-icons/gi";
 
+let date_time = new Date();
+let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+let date = ("0" + date_time.getDate()).slice(-2);
+let day = date_time.getDay();
+const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
 const Home: React.FC = () => {
     return (
         <div>
@@ -25,30 +32,30 @@ const Home: React.FC = () => {
                 <ExploreContainer />
             </IonContent> */}
             <div className="grid grid-cols-2 min-w-full p-2 gap-3">
-                <div className="flex flex-col p-2 gap-2 relative bg-black/30 min-w-1/2 min-h-[33%] aspect-square rounded-2xl  overflow-hidden">
-                    <div className="text-3xl font-bold text-accent">Friday</div>
-                    <div className="text-lg">5.01</div>
-                    <div className="absolute text-accent text-[120px] -bottom-2 -right-2 rotate-45"><FaCalendarAlt /></div>
+                <div className="flex flex-col p-3 gap-1 relative bg-black/30 min-w-1/2 min-h-[33%] aspect-square rounded-2xl  overflow-hidden">
+                    <div className="text-2xl font-bold text-accent">{weekDays[day-1]}</div>
+                    <div className="text-4xl font-light">{date}.{month}</div>
+                    <div className="absolute text-accent text-[130px] -bottom-5 -right-2 rotate-45 -z-10"><FaCalendarAlt /></div>
                 </div>
-                <div className="flex flex-col p-2 gap-2 relative bg-black/30 min-w-1/2 min-h-[33%] aspect-square rounded-2xl">
+                <div className="flex flex-col p-3 gap-1 relative bg-black/30 min-w-1/2 min-h-[33%] aspect-square rounded-2xl">
                     <div className="text-3xl font-bold text-accent">Journal</div>
-                    <div className="opacity-40">NO ENTRIES TODAY</div>
+                    <div className="opacity-40 text-sm">NO ENTRIES TODAY</div>
                 </div>
-                <div className="flex flex-col p-2 gap-2 relative bg-black/30 min-w-1/2 min-h-[33%] aspect-square rounded-2xl">
+                <div className="flex flex-col p-3 gap-1 relative bg-black/30 min-w-1/2 min-h-[33%] aspect-square rounded-2xl">
                     <div className="text-3xl font-bold text-accent">Tasks</div>
-                    <div className="opacity-40">NO TASKS ADDED</div>
+                    <div className="opacity-40 text-sm">NO TASKS ADDED</div>
                 </div>
-                <div className="flex flex-col p-2 gap-2 relative bg-black/30 min-w-1/2 min-h-[33%] aspect-square rounded-2xl overflow-hidden">
+                <div className="flex flex-col p-3 gap-1 relative bg-black/30 min-w-1/2 min-h-[33%] aspect-square rounded-2xl overflow-hidden">
                     <div className="text-3xl font-bold text-accent">Health</div>
                     <div className="text-lg">1235 steps</div>
-                    <div className="absolute text-accent text-[110px] -bottom-5 -right-0 rotate-[30deg]"><BsFillHeartPulseFill /></div>
+                    <div className="absolute text-accent text-[110px] -bottom-5 -right-0 rotate-[30deg] -z-10"><BsFillHeartPulseFill /></div>
                 </div>
             </div>
-            <div className="flex flex-row p-2 m-2 gap-2 relative bg-black/30 min-w-1/2 min-h-[33%] rounded-2xl items-center">
+            <div className="flex flex-row p-3 m-2 gap-1 relative bg-black/30 min-w-1/2 min-h-[33%] rounded-2xl items-center">
                 <div className="text-accent text-[110px]"><GiJugglingSeal /></div>
                 <div className="flex flex-col">
                     <div className="text-3xl font-bold text-accent">Seal Quote</div>
-                    <div className="">"Success is not final, failure is not fatal: It is the courage to continue that counts." — Winston S. Churchill</div>
+                    <div className="opacity-40 text-sm">"Success is not final, failure is not fatal: It is the courage to continue that counts." — Winston S. Churchill</div>
                 </div>
             </div>        
         </div>        
