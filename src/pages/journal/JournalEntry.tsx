8508294e5d5dacc68817add6e9f3b5420b7f7919
boxiercarpet <boxiercarpet@gmail.com>
2024-08-08 21:37:12 +0200

    feat: Update styling and Improve Journal

--- src/pages/journal/JournalEntry.tsx
+++ src/pages/journal/JournalEntry.tsx
@@ -1,6 +1,12 @@
 import { Camera, CameraResultType } from "@capacitor/camera";
 import { Keyboard } from "@capacitor/keyboard";
-import { IonHeader, IonToolbar, useIonRouter } from "@ionic/react";
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonToolbar,
+    useIonRouter,
+} from "@ionic/react";
 import { useEffect, useState } from "react";
 import { IconType } from "react-icons";
 import {
@@ -18,19 +24,40 @@ import {
     FaFaceSmile,
 } from "react-icons/fa6";
 import { Moods } from "../../types/journal";
-import { useDispatch } from "react-redux";
-import { addJournalEntry } from "../../store/journal";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    addJournalEntry,
+    selectJournalEntires,
+    updateJournalEntry,
+} from "../../store/journal";
+import { RouteComponentProps, useHistory } from "react-router";
 
-const JournalEntry: React.FC = () => {
+interface JournalEntryPageProps
+    extends RouteComponentProps<{
+        id?: string;
+    }> {}
+
+const JournalEntry: React.FC<JournalEntryPageProps> = ({ match }) => {
+    const entries = useSelector(selectJournalEntires);
+    const id = match.params.id ? parseInt(match.params.id) : undefined;
+    const entry = id ? entries[id] : null;
     const dispatch = useDispatch();
     const router = useIonRouter();
     const [keyboardVisible, setKeyboardVisible] = useState(false);
     const [content, setContent] = useState("");
     const [mood, setMood] = useState<Moods>(Moods.OKAY);
+    const [title, setTitle] = useState("New Entry");
     const [photo, setPhoto] = useState<string | null>(null);
     const [favorite, setFavorite] = useState(false);
 
     useEffect(() => {
+        if (entry) {
+            setTitle(entry.title);
+            setContent(entry.content);
+            setMood(entry.mood);
+            setFavorite(entry.favorite);
+        }
+
         Keyboard.addListener("keyboardWillShow", () => {
             setKeyboardVisible(true);
         });
@@ -79,33 +106,48 @@ const JournalEntry: React.FC = () => {
         );
     };
 
-    const newEntry = () => {
-        dispatch(
-            addJournalEntry({
-                title: "New Entry",
-                content,
-                mood,
-                date: new Date().getTime(),
-                favorite,
-            })
-        );
-        router.push("/journal");
+    const saveEntry = () => {
+        console.log(entry, id);
+        if (entry && id !== undefined) {
+            dispatch(
+                updateJournalEntry({
+                    index: id,
+                    title,
+                    content,
+                    mood,
+                    favorite,
+                })
+            );
+        } else {
+            dispatch(
+                addJournalEntry({
+                    title,
+                    content,
+                    mood,
+                    date: new Date().getTime(),
+                    favorite,
+                })
+            );
+        }
+        router.goBack();
     };
 
     return (
-        <>
-            <IonHeader className="shadow-none bg-primary/20">
+        <IonPage>
+            <IonHeader className="shadow-none">
                 <IonToolbar>
-                    <div className="flex justify-between items-center px-2 pl-3 text-xl font-bold gap-1">
+                    <div className="flex justify-between items-center px-2 pl-3 text-xl font-bold gap-1 h-10">
                         <div
                             className="flex justify-center items-center h-10 w-10"
                             onClick={() => router.goBack()}
                         >
                             <FaArrowLeft />
                         </div>
-                        <div className="py-2 flex-1 outline-none overflow-x-auto whitespace-nowrap">
-                            New Entry
-                        </div>
+                        <input
+                            className="py-2 flex-1 outline-none overflow-x-auto whitespace-nowrap bg-transparent"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
                         <div
                             className="flex justify-center items-center h-10 w-10"
                             onClick={() => setFavorite((c) => !c)}
@@ -119,80 +161,82 @@ const JournalEntry: React.FC = () => {
                     </div>
                 </IonToolbar>
             </IonHeader>
-            <div className="p-3 flex flex-col gap-3 w-full flex-1 shrink-0 h-0">
-                <div className="flex flex-col gap-3">
-                    <div className="text-xl font-bold px-2">
-                        How are you feeling today?
+            <IonContent scrollY={false}>
+                <div className="p-3 flex flex-col gap-3 w-full h-full">
+                    <div className="flex flex-col gap-3">
+                        <div className="text-xl font-bold px-2">
+                            How are you feeling today?
+                        </div>
+                        <div className="flex justify-evenly font-bold text-xs text-white/40">
+                            <Mood
+                                label="Terrible"
+                                icon={FaFaceFrown}
+                                className="text-red-500"
+                                name={Moods.TERRIBLE}
+                            />
+                            <Mood
+                                label="Bad"
+                                icon={FaFaceFrownOpen}
+                                className="text-orange-500"
+                                name={Moods.BAD}
+                            />
+                            <Mood
+                                label="Okay"
+                                icon={FaFaceMeh}
+                                className="text-yellow-500"
+                                name={Moods.OKAY}
+                            />
+                            <Mood
+                                label="Good"
+                                icon={FaFaceSmile}
+                                className="text-green-500"
+                                name={Moods.GOOD}
+                            />
+                            <Mood
+                                label="Great"
+                                icon={FaFaceLaugh}
+                                className="text-green-700"
+                                name={Moods.GREAT}
+                            />
+                        </div>
                     </div>
-                    <div className="flex justify-evenly font-bold text-xs text-white/40">
-                        <Mood
-                            label="Terrible"
-                            icon={FaFaceFrown}
-                            className="text-red-500"
-                            name={Moods.TERRIBLE}
+                    <div className="flex-1 shrink-0 h-0 overflow-auto bg-primary/40 rounded-2xl p-3 outline-none relative">
+                        <textarea
+                            className="w-full h-full bg-transparent outline-none placeholder:text-white/40 placeholder:font-bold"
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                            placeholder="Write something you would like to share..."
                         />
-                        <Mood
-                            label="Bad"
-                            icon={FaFaceFrownOpen}
-                            className="text-orange-500"
-                            name={Moods.BAD}
-                        />
-                        <Mood
-                            label="Okay"
-                            icon={FaFaceMeh}
-                            className="text-yellow-500"
-                            name={Moods.OKAY}
-                        />
-                        <Mood
-                            label="Good"
-                            icon={FaFaceSmile}
-                            className="text-green-500"
-                            name={Moods.GOOD}
-                        />
-                        <Mood
-                            label="Great"
-                            icon={FaFaceLaugh}
-                            className="text-green-700"
-                            name={Moods.GREAT}
-                        />
+                        {photo && (
+                            <img
+                                src={`data:image/jpeg;base64,${photo}`}
+                                alt="photo"
+                                className="w-full aspect-w-16 aspect-h-9 rounded-lg mt-3"
+                            />
+                        )}
                     </div>
-                </div>
-                <div className="flex-1 shrink-0 h-0 overflow-auto bg-primary/40 rounded-2xl p-3 outline-none relative">
-                    <textarea
-                        className="w-full h-full bg-transparent outline-none placeholder:text-white/40 placeholder:font-bold"
-                        value={content}
-                        onChange={(e) => setContent(e.target.value)}
-                        placeholder="Write something you would like to share..."
-                    />
-                    {photo && (
-                        <img
-                            src={`data:image/jpeg;base64,${photo}`}
-                            alt="photo"
-                            className="w-full aspect-w-16 aspect-h-9 rounded-lg mt-3"
-                        />
+                    {!keyboardVisible && (
+                        <div className="flex gap-2 h-12">
+                            <div
+                                className="bg-primary/30 rounded-full flex-[1] flex justify-center items-center text-xl"
+                                onClick={selectPhoto}
+                            >
+                                <FaImage />
+                            </div>
+                            <div className="bg-primary/30 rounded-full flex-[1] flex justify-center items-center text-xl">
+                                <FaMicrophone />
+                            </div>
+                            <div
+                                className="bg-accent rounded-full flex-[2] flex justify-center items-center text-lg font-bold uppercase"
+                                onClick={saveEntry}
+                            >
+                                save
+                            </div>
+                        </div>
                     )}
                 </div>
-                {!keyboardVisible && (
-                    <div className="flex gap-2 h-12">
-                        <div
-                            className="bg-primary/30 rounded-full flex-[1] flex justify-center items-center text-xl"
-                            onClick={selectPhoto}
-                        >
-                            <FaImage />
-                        </div>
-                        <div className="bg-primary/30 rounded-full flex-[1] flex justify-center items-center text-xl">
-                            <FaMicrophone />
-                        </div>
-                        <div
-                            className="bg-accent rounded-full flex-[2] flex justify-center items-center text-lg font-bold uppercase"
-                            onClick={newEntry}
-                        >
-                            save
-                        </div>
-                    </div>
-                )}
-            </div>
-        </>
+            </IonContent>
+        </IonPage>
     );
 };
 
--- src/pages/journal/index.tsx
+++ src/pages/journal/index.tsx
@@ -1,16 +1,17 @@
-import { IonRouterOutlet } from "@ionic/react";
+import { IonPage, IonRouterOutlet } from "@ionic/react";
 import { Route } from "react-router";
 import JournalList from "./JournalList";
 import JournalEntry from "./JournalEntry";
 
 const Journal: React.FC = () => {
     return (
-        <div>
+        <IonPage>
             <IonRouterOutlet className="flex flex-col overflow-hidden">
                 <Route exact path="/journal" component={JournalList} />
                 <Route exact path="/journal/add" component={JournalEntry} />
+                <Route path="/journal/edit/:id" component={JournalEntry} />
             </IonRouterOutlet>
-        </div>
+        </IonPage>
     );
 };
 
