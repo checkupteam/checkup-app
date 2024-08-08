fef68847bd7f9d2f960d046cf06663b62f2df411
PAtreju <pq5978@gmail.com>
2024-08-08 12:41:03 +0200

    datepicker

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { FaSmile, FaRegStar, FaStar, FaBed } from "react-icons/fa";
 import { IonHeader, IonToolbar } from "@ionic/react";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 
 function nearestMonday() {
     let day = now.getDay();
@@ -18,7 +22,6 @@ function daysInPrevMonth() {
 
 function getDay() {
     let day = now.getDay();
-
     return weekDays[day - 1];
 }
 
@@ -54,21 +57,9 @@ function getFirstDayName() {
     return firstDay;
 }
 
+
 let now = new Date();
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const month = months[new Date().getMonth()];
 const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 const year = now.getFullYear();
@@ -76,8 +67,10 @@ const day_month = Array.from({ length: daysInThisMonth() }, (_, i) => i + 1);
 const dates_month = [...addPrevDays(), ...day_month, ...addLastDays()];
 const dates_week = dates_month.slice(nearestMonday() - 1, nearestMonday() + 6);
 
+
 const Calendar: React.FC = () => {
     const [view, setView] = useState("month");
+    const displayedMonth = useState(now.getMonth());
     return (
         <div>
             <IonHeader>
@@ -85,24 +78,10 @@ const Calendar: React.FC = () => {
                     <div className="flex justify-between items-center px-2 pl-3 text-xl font-bold">
                         <div className="py-2">Calendar</div>
                         <div className="flex gap-1">
-                            <button
-                                className={`flex gap-2 px-2 p-1 items-center w-fit rounded-xl text-sm h-fit ${
-                                    view === "month"
-                                        ? "bg-accent"
-                                        : "bg-white/10"
-                                }`}
-                                onClick={() => setView("month")}
-                            >
+                            <button className={`flex gap-2 px-2 p-1 items-center w-fit rounded-xl text-sm h-fit ${view === "month" ? "bg-accent" : "bg-white/10"}`} onClick={() => setView("month")}>
                                 Month
                             </button>
-                            <button
-                                className={`flex gap-1 px-2 p-1 items-center w-fit rounded-xl text-sm h-fit ${
-                                    view === "week"
-                                        ? "bg-accent"
-                                        : "bg-white/10"
-                                }`}
-                                onClick={() => setView("week")}
-                            >
+                            <button className={`flex gap-1 px-2 p-1 items-center w-fit rounded-xl text-sm h-fit ${view === "week" ? "bg-accent" : "bg-white/10"}`} onClick={() => setView("week")}>
                                 Week
                             </button>
                         </div>
@@ -112,47 +91,24 @@ const Calendar: React.FC = () => {
             <div className="p-4">
                 <div className="grid grid-cols-7 text-center text-xs mb-2">
                     {weekDays.map((day, index) => (
-                        <div
-                            key={index}
-                            className={`${
-                                getDay() === day ? "text-accent" : "text-white"
-                            } font-bold`}
-                        >
+                        <div key={index} className={`${getDay() === day ? "text-accent" : "text-white"} font-bold`}>
                             {day}
                         </div>
                     ))}
                 </div>
-                <div
-                    className={`grid grid-cols-7 text-center text-lg border-gray-700 ${
-                        view === "month" ? "border-0" : "border-b"
-                    }`}
-                >
-                    {(view === "month" ? dates_month : dates_week).map(
-                        (date, index) => (
-                            <div
-                                key={index}
-                                className={`${
-                                    date === now.getDate()
-                                        ? "text-accent"
-                                        : "text-white" &&
-                                          (index < getFirstDayName() - 1 ||
-                                              index > lackingDays() - 1) &&
-                                          view === "month"
-                                        ? "opacity-30"
-                                        : "opacity-100"
-                                } p-1 font-bold`}
-                            >
-                                {date}
-                            </div>
-                        )
-                    )}
+                <div className={`grid grid-cols-7 text-center text-lg border-gray-700 ${view === "month" ? "border-0" : "border-b"}`}>
+                    {(view === "month" ? dates_month : dates_week).map((date, index) => (
+                        <div key={index} className={`${date === now.getDate() ? "text-accent" : "text-white" && (index < getFirstDayName() - 1 || index > lackingDays() - 1) && view === "month" ? "opacity-30" : "opacity-100"} p-1 font-bold`}>
+                            {date}
+                        </div>
+                    ))}
                 </div>
-                <div
-                    className={`w-full justify-center border-b border-gray-700 p-2 font-bold text-xl ${
-                        view === "month" ? "flex" : "hidden"
-                    }`}
-                >
-                    {month} {year}
+                <div className={`w-full justify-center border-b border-gray-700 p-2 font-bold text-xl ${view === "month" ? "flex" : "hidden"}`}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DemoContainer components={['DatePicker']}>
+                            <DatePicker label={'"month" and "year"'} views={['month', 'year']} />
+                        </DemoContainer>
+                    </LocalizationProvider>
                 </div>
             </div>
             <div className="flex flex-col gap-3 px-4">
@@ -163,9 +119,7 @@ const Calendar: React.FC = () => {
                         </div>
                         <div className="flex flex-col flex-1">
                             <div className="text-xl font-bold">Title</div>
-                            <div className="text-lg opacity-45 leading-5">
-                                date
-                            </div>
+                            <div className="text-lg opacity-45 leading-5">date</div>
                         </div>
                         <div className="self-stretch w-11 aspect-square flex justify-center items-center text-2xl opacity-45">
                             <FaRegStar />
