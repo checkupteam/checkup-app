c87a91ac4cbcc1ccc80400be9966999eaba01ba8
boxiercarpet <boxiercarpet@gmail.com>
2024-08-13 19:41:56 +0200

    feat: calendar element work

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,56 +1,46 @@
 import React, { useRef, useState } from "react";
-import { FaSmile, FaRegStar, FaStar, FaBed } from "react-icons/fa";
+import { FaSmile, FaRegStar, FaBed } from "react-icons/fa";
 import { IonHeader, IonToolbar } from "@ionic/react";
-import { IonDatetime, IonDatetimeButton, IonButton, IonButtons, IonModal } from "@ionic/react";
+import { IonDatetime, IonDatetimeButton, IonModal } from "@ionic/react";
+
+const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 const Calendar: React.FC = () => {
-    function getDay() {
-        let day = now.getDay();
+    const [currDate, setDate] = useState(new Date());
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const datetime = useRef<null | HTMLIonDatetimeElement>(null);
+    const now = new Date();
+
+    function getWeekDay() {
+        let day = selectedDate.getDay();
         return weekDays[day - 1];
     }
 
-    function displayCalendar(month: number = now.getMonth() + 1, year: number = now.getFullYear()) {
-        const firstDay = new Date(year, month - 1, 0);
-        const lastDay = new Date(year, month, 0);
-        const firstDayWeek = firstDay.getDay();
-        const days = lastDay.getDate();
+    function displayCalendar() {
+        const month = currDate.getMonth();
+        const year = currDate.getFullYear();
+        const firstWeekDay = new Date(year, month, 1).getDay() - 1;
+        const days = new Date(year, month + 1, 0).getDate();
         const calendar = [];
-        let day_now = 1;
-        let day_prev = new Date(year, month, 0).getDate() - firstDayWeek + 1;
-        let day_next = 1;
-        for (let i = 0; i < 6; i++) {
-            for (let j = 0; j < 7; j++) {
-                if (i === 0 && j < firstDayWeek) {
-                    calendar.push("a" + day_prev);
-                    day_prev++;
-                } else if (day_now > days) {
-                    calendar.push("a" + day_next);
-                    day_next++;
+
+        for (let i = 1; i <= 5; i++) {
+            for (let j = 1; j <= 7; j++) {
+                const x = 7 * (i - 1) + j;
+
+                if (i === 1 && j <= firstWeekDay) {
+                    calendar.push(new Date(year, month, 0 - firstWeekDay + j));
+                } else if (7 * (i - 1) + j - firstWeekDay > days) {
+                    calendar.push(
+                        new Date(year, month + 1, x - days - firstWeekDay)
+                    );
                 } else {
-                    calendar.push(String(day_now));
-                    day_now++;
+                    calendar.push(new Date(year, month, x - firstWeekDay));
                 }
             }
         }
+
         return calendar;
     }
-
-    const confirm = () => {
-        console.log("confirm");
-    };
-    const cancel = () => {
-        console.log("cancel");
-    };
-
-    let now = new Date();
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-    const month_now = months[new Date().getMonth()];
-    let displayedMonth = displayCalendar();
-
-    const modal = useRef<HTMLIonModalElement>(null);
-    const [value, setValue] = useState(new Date().toISOString());
 
     return (
         <div>
@@ -62,29 +52,67 @@ const Calendar: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <div className="p-4">
-                <div className="grid grid-cols-7 text-center text-xs mb-2">
+                <div className="grid grid-cols-7 text-center text-xs mb-2 gap-3">
                     {weekDays.map((day, index) => (
-                        <div key={index} className={`${getDay() === day ? "text-accent" : "text-white"} font-bold`}>
+                        <div
+                            key={index}
+                            className={`${
+                                getWeekDay() === day
+                                    ? "text-accent"
+                                    : "text-white"
+                            } font-bold`}
+                        >
                             {day}
                         </div>
                     ))}
                 </div>
-                <div className={`grid grid-cols-7 text-center text-lg`}>
-                    {displayedMonth.map((date, index) => (
-                        <div key={index} className={`${date.includes("a") ? "text-white/20" : "text-white"}`}>
-                            {date.includes("a") ? date.replace("a", "") : date}
+                <div
+                    className={`grid grid-cols-7 text-center text-lg gap-x-3 mb-1`}
+                >
+                    {displayCalendar().map((date, index) => (
+                        <div
+                            key={index}
+                            className={`aspect-square content-center text-center rounded-full transition-colors duration-200 ${
+                                date.toDateString() ==
+                                selectedDate.toDateString()
+                                    ? "bg-accent"
+                                    : date.getMonth() != currDate.getMonth()
+                                    ? "text-white/20"
+                                    : "text-white font-semibold"
+                            }`}
+                            onClick={() => setSelectedDate(date)}
+                        >
+                            {date.getDate()}
                         </div>
                     ))}
                 </div>
-                <div className={`flex border-b w-full justify-center border-gray-700 p-2`}>
-                    <IonDatetimeButton datetime="datetime" ></IonDatetimeButton>
+                <div
+                    className={`flex border-b w-full justify-center border-white/10 p-3`}
+                >
+                    <IonDatetimeButton datetime="datetime"></IonDatetimeButton>
 
-                    <IonModal keepContentsMounted={true} ref={modal} trigger="open-modal" onDidDismiss={({ detail }) => console.log("didDismiss", JSON.stringify(detail))}>
-                        <IonDatetime id="datetime" presentation="month-year" value={value} onIonChange={( ) => setValue(value)}>
-                            <IonButtons slot="buttons">
-                                <IonButton onClick={() => modal.current!.dismiss(null, "cancel")}>Cancel</IonButton>
-                                <IonButton onClick={() => modal.current!.dismiss(value, 'confirm')}>Confirm</IonButton>
-                            </IonButtons>
+                    <IonModal keepContentsMounted={true}>
+                        <IonDatetime
+                            id="datetime"
+                            presentation="month-year"
+                            ref={datetime}
+                            value={currDate.toISOString()}
+                            onIonChange={({ detail }) =>
+                                setDate((c) =>
+                                    typeof detail.value == "string"
+                                        ? new Date(detail.value)
+                                        : c
+                                )
+                            }
+                        >
+                            {/* <IonButtons slot="buttons">
+                <IonButton onClick={() => datetime.current?.cancel(true)}>
+                  Cancel
+                </IonButton>
+                <IonButton onClick={() => datetime.current?.confirm(true)}>
+                  Confirm
+                </IonButton>
+              </IonButtons> */}
                         </IonDatetime>
                     </IonModal>
                 </div>
@@ -97,7 +125,9 @@ const Calendar: React.FC = () => {
                         </div>
                         <div className="flex flex-col flex-1">
                             <div className="text-xl font-bold">Title</div>
-                            <div className="text-lg opacity-45 leading-5">date</div>
+                            <div className="text-lg opacity-45 leading-5">
+                                date
+                            </div>
                         </div>
                         <div className="self-stretch w-11 aspect-square flex justify-center items-center text-2xl opacity-45">
                             <FaRegStar />
