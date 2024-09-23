e37c22241059f4062bab7d67bc1d0210140de3f6
boxiercarpet <boxiercarpet@gmail.com>
2024-09-23 21:24:09 +0200

    chore: Update calendar design

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,22 +1,22 @@
-import React, { useRef, useState } from "react";
-import { FaSmile, FaRegStar, FaBed, FaStar } from "react-icons/fa";
+import React, { useRef, useState } from 'react';
+import { FaSmile, FaRegStar, FaBed, FaStar } from 'react-icons/fa';
 import {
     IonContent,
     IonHeader,
     IonPage,
     IonToolbar,
     useIonRouter,
-} from "@ionic/react";
-import { IonDatetime, IonDatetimeButton, IonModal } from "@ionic/react";
-import { JournalEntry } from "../types/journal";
-import Mood from "../components/Mood";
+} from '@ionic/react';
+import { IonDatetime, IonDatetimeButton, IonModal } from '@ionic/react';
+import { JournalEntry } from '../types/journal';
+import Mood from '../components/Mood';
 import {
     useGetJournalEntriesQuery,
     useUpdateJournalEntryMutation,
-} from "../api/journal";
-import Loading from "../components/Loading";
+} from '../api/journal';
+import Loading from '../components/Loading';
 
-const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
 const JournalItem: React.FC<{ entry: JournalEntry }> = ({ entry }) => {
     const router = useIonRouter();
@@ -44,12 +44,12 @@ const JournalItem: React.FC<{ entry: JournalEntry }> = ({ entry }) => {
                     </div>
                     <div className="text-neutral-500 leading-5 font-semibold">
                         {new Date(entry.createdAt)
-                            .toLocaleString("pl-PL", {
-                                month: "2-digit",
-                                day: "2-digit",
-                                year: "numeric",
+                            .toLocaleString('pl-PL', {
+                                month: '2-digit',
+                                day: '2-digit',
+                                year: 'numeric',
                             })
-                            .replaceAll("/", ".")}
+                            .replaceAll('/', '.')}
                     </div>
                 </div>
             </div>
@@ -102,7 +102,7 @@ const Calendar: React.FC = () => {
                     calendar.push(new Date(year, month, 0 - firstWeekDay + j));
                 } else if (7 * (i - 1) + j - firstWeekDay > days) {
                     calendar.push(
-                        new Date(year, month + 1, x - days - firstWeekDay)
+                        new Date(year, month + 1, x - days - firstWeekDay),
                     );
                 } else {
                     calendar.push(new Date(year, month, x - firstWeekDay));
@@ -119,6 +119,7 @@ const Calendar: React.FC = () => {
                 <IonToolbar>
                     <div className="flex justify-between items-center px-2 pl-3 text-xl font-bold text-white">
                         <div className="py-2">Calendar</div>
+                        <IonDatetimeButton datetime="datetime"></IonDatetimeButton>
                     </div>
                 </IonToolbar>
             </IonHeader>
@@ -130,8 +131,8 @@ const Calendar: React.FC = () => {
                                 key={index}
                                 className={`${
                                     getWeekDay() === day
-                                        ? "text-accent"
-                                        : "text-white"
+                                        ? 'text-accent'
+                                        : 'text-white'
                                 } font-bold`}
                             >
                                 {day}
@@ -147,10 +148,10 @@ const Calendar: React.FC = () => {
                                 className={`aspect-square content-center text-center rounded-full transition-colors duration-200 ${
                                     date.toDateString() ==
                                     selectedDate.toDateString()
-                                        ? "bg-accent"
+                                        ? 'bg-accent'
                                         : date.getMonth() != currDate.getMonth()
-                                        ? "text-white/20"
-                                        : "text-white font-semibold"
+                                          ? 'text-white/20'
+                                          : 'text-white font-semibold'
                                 }`}
                                 onClick={() => setSelectedDate(date)}
                             >
@@ -161,19 +162,18 @@ const Calendar: React.FC = () => {
                     <div
                         className={`flex border-b w-full justify-center border-white/10 p-3`}
                     >
-                        <IonDatetimeButton datetime="datetime"></IonDatetimeButton>
-
                         <IonModal keepContentsMounted={true}>
                             <IonDatetime
                                 id="datetime"
+                                className="bg-darker-violet-800"
                                 presentation="month-year"
                                 ref={datetime}
                                 value={currDate.toISOString()}
                                 onIonChange={({ detail }: { detail: any }) =>
                                     setDate((c) =>
-                                        typeof detail.value == "string"
+                                        typeof detail.value == 'string'
                                             ? new Date(detail.value)
-                                            : c
+                                            : c,
                                     )
                                 }
                             >
