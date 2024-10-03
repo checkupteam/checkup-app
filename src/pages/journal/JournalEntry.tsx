66c2cc0dd99f51fc849313f809bfae09ba99f990
boxiercarpet <boxiercarpet@gmail.com>
2024-10-03 19:25:01 +0200

    feat: no work :)

--- src/pages/journal/JournalEntry.tsx
+++ src/pages/journal/JournalEntry.tsx
@@ -8,7 +8,7 @@ import {
     useIonRouter,
     useIonViewWillEnter,
 } from '@ionic/react';
-import { useEffect, useState } from 'react';
+import { startTransition, useEffect, useState } from 'react';
 import { IconType } from 'react-icons';
 import {
     FaArrowLeft,
@@ -53,6 +53,11 @@ const JournalEntry: React.FC<JournalEntryPageProps> = ({ match }) => {
     const [mood, setMood] = useState<Moods>(Moods.OKAY);
     const [title, setTitle] = useState('New Entry');
     const [favorite, setFavorite] = useState(false);
+    const [haveDreams, setHaveDreams] = useState(false);
+    const [dreamText, setDreamText] = useState('');
+    const [sleepRating, setSleepRating] = useState<String>(
+        Moods.OKAY.toString(),
+    );
     const [changeMood, setChangeMood] = useState(false);
     const [updateJournalEntry] = useUpdateJournalEntryMutation();
 
@@ -87,6 +92,9 @@ const JournalEntry: React.FC<JournalEntryPageProps> = ({ match }) => {
             setMood(entry.mood);
             setTitle(entry.title);
             setFavorite(entry.isFavorite);
+            if (entry.Answer?.length > 0) {
+                setSleepRating(entry.Answer[1].answer);
+            }
         }
     }, [entry]);
 
@@ -162,7 +170,9 @@ const JournalEntry: React.FC<JournalEntryPageProps> = ({ match }) => {
                                             className="flex flex-col gap-1 items-center w-fit text-2xl font-semibold text-white/40 z-10"
                                             layoutId={mood + '-journal-edit'}
                                             onClick={() =>
-                                                setChangeMood((c) => !c)
+                                                startTransition(() => {
+                                                    setChangeMood((c) => !c);
+                                                })
                                             }
                                         >
                                             <Mood
@@ -172,37 +182,34 @@ const JournalEntry: React.FC<JournalEntryPageProps> = ({ match }) => {
                                         </motion.div>
                                     ),
                             )}
-                            {changeMood && (
+                            {/* {changeMood && (
                                 <div className="absolute -bottom-2 flex gap-2">
-                                    {Object.values(Moods).map(
-                                        (mood) =>
-                                            (mood as Moods) && (
-                                                <motion.div
-                                                    key={mood}
-                                                    className="flex flex-col gap-1 items-center w-fit text-2xl font-semibold text-white/40"
-                                                    layoutId={
-                                                        mood + '-journal-edit'
-                                                    }
-                                                    onClick={() => {
-                                                        // setMood(mood);
-                                                        setChangeMood(false);
-                                                    }}
-                                                >
-                                                    {/* <Mood
-                                                mood={mood as Moods}
-                                                className={'text-6xl'}
-                                            /> */}
-                                                    <div>
-                                                        {t(
-                                                            'journal.mood.' +
-                                                                mood,
-                                                        )}
-                                                    </div>
-                                                </motion.div>
-                                            ),
-                                    )}
+                                    {Object.values(Moods)
+                                        .filter(
+                                            (mood) => typeof mood === 'number',
+                                        )
+                                        .map((mood) => (
+                                            <motion.div
+                                                key={mood}
+                                                className="flex flex-col gap-1 items-center w-fit text-2xl font-semibold text-white/40"
+                                                layoutId={
+                                                    mood + '-journal-edit'
+                                                }
+                                                onClick={() => {
+                                                    setChangeMood(false);
+                                                }}
+                                            >
+                                                <Mood
+                                                    mood={mood}
+                                                    className={'text-6xl'}
+                                                />
+                                                <div>
+                                                    {t('journal.mood.' + mood)}
+                                                </div>
+                                            </motion.div>
+                                        ))}
                                 </div>
-                            )}
+                            )} */}
                             <div className="rounded-xl bg-darker-violet-850 min-h-28 flex-1 p-2 px-3 pr-14 relative font-semibold overflow-hidden animate-fadeIn">
                                 <div>
                                     {t('journal.motivation.' + entry?.mood)}
@@ -215,30 +222,95 @@ const JournalEntry: React.FC<JournalEntryPageProps> = ({ match }) => {
                             </div>
                             <div className="w-full h-0.5 bg-darker-violet-850"></div>
                         </div>
-
-                        <div
-                            className="flex-1 shrink-0 h-0 overflow-auto bg-primary/40 rounded-2xl p-3 outline-none relative pb-32"
-                            contentEditable
-                            dangerouslySetInnerHTML={{ __html: content }}
-                            onBlur={(e) =>
-                                setContent(e.currentTarget.innerHTML)
-                            }
-                        >
-                            {/* <textarea
+                        <div className="flex-1 h-0 flex flex-col gap-3 overflow-auto">
+                            <div className="text-2xl font-bold px-2">
+                                {t('journal.question.2')}
+                            </div>
+                            <textarea
+                                className="rounded-xl bg-darker-violet-950 w-full min-h-36 p-2 px-3 font-semibold resize-none outline-none scroll-mt-12"
+                                // value={firstQuestion}
+                                // onChange={(event) =>
+                                //     setFirstQuestion(event.target.value)
+                                // }
+                            />
+                            <div className="text-2xl font-bold px-2 mt-2">
+                                {t('journal.question.3')}
+                            </div>
+                            <div className="flex flex-row gap-5 items-center justify-center w-fulls">
+                                {Object.values(Moods)
+                                    .filter((mood) => typeof mood === 'number')
+                                    .map((mood) => (
+                                        <div
+                                            key={mood}
+                                            className={`flex flex-col items-center justify-center ${sleepRating == mood.toString() ? 'opacity-100' : 'opacity-40'} `}
+                                            onClick={() =>
+                                                setSleepRating(mood.toString())
+                                            }
+                                        >
+                                            <Mood
+                                                mood={mood}
+                                                className="text-5xl"
+                                            />
+                                            <div>
+                                                {t('journal.mood.' + mood)}
+                                            </div>
+                                        </div>
+                                    ))}
+                            </div>
+                            {/* <textarea className="rounded-xl bg-darker-violet-950 w-full min-h-36 p-2 px-3 font-semibold resize-none outline-none scroll-mt-12" /> */}
+                            <div className="text-2xl font-bold px-2 mt-2">
+                                {t('journal.question.4')}
+                            </div>
+                            <div className="flex flex-row items-center justify-center gap-2 h-10">
+                                <div
+                                    className={`rounded-full flex-1 h-full flex justify-center items-center transition-colors text-lg font-bold uppercase ${haveDreams == true ? 'bg-accent' : 'bg-darker-violet-800'}`}
+                                    // onClick={() => setHaveDreams(true)}
+                                >
+                                    YES
+                                </div>
+                                <div
+                                    className={`rounded-full flex-1 h-full flex justify-center items-center transition-colors text-lg font-bold uppercase ${haveDreams == false ? 'bg-accent' : 'bg-darker-violet-800'}`}
+                                    // onClick={() => setHaveDreams(false)}
+                                >
+                                    NO
+                                </div>
+                            </div>
+                            {/* {haveDreams == true && (
+                                <textarea
+                                    className={`rounded-xl bg-darker-violet-950 w-full min-h-36 p-2 px-3 font-semibold resize-none outline-none scroll-mt-12`}
+                                    value={dreamText}
+                                    onChange={(event) =>
+                                        setDreamText(event.target.value)
+                                    }
+                                />
+                            )} */}
+                            <div className="text-2xl font-bold px-2 mt-2">
+                                {t('journal.question.5')}
+                            </div>
+                            <div
+                                className="shrink-0 min-h-64 overflow-auto bg-primary/40 rounded-2xl p-3 outline-none relative pb-32"
+                                contentEditable
+                                dangerouslySetInnerHTML={{ __html: content }}
+                                onBlur={(e) =>
+                                    setContent(e.currentTarget.innerHTML)
+                                }
+                            >
+                                {/* <textarea
                             className="w-full h-full bg-transparent outline-none placeholder:text-white/40 placeholder:font-bold resize-none"
                             value={content}
                             onChange={(e) => setContent(e.target.value)}
                             placeholder="Write something you would like to share..."
                         /> */}
-                            {/* {photo && (
+                                {/* {photo && (
                             <img
                                 src={`data:image/jpeg;base64,${photo}`}
                                 alt="photo"
                                 className="w-full aspect-w-16 aspect-h-9 rounded-lg mt-3"
                             />
                         )} */}
+                            </div>
                         </div>
-                        {!keyboardVisible && (
+                        {/* {!keyboardVisible && (
                             <div className="flex gap-2 h-12">
                                 <div
                                     className="bg-primary/30 rounded-full flex-[1] flex justify-center items-center text-xl"
@@ -256,7 +328,7 @@ const JournalEntry: React.FC<JournalEntryPageProps> = ({ match }) => {
                                     save
                                 </div>
                             </div>
-                        )}
+                        )} */}
                     </div>
                 )}
             </IonContent>
